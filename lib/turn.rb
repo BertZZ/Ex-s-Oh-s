2196b00a96905b20cef2531199d0481bcf372bdc
Bertie Wooles <bertie.wooles@hotmail.co.uk>
2017-06-14 18:01:54 +0100

    Wrote a horribly long if/else statment to turn user inputed number into array coordanates

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,8 +4,41 @@ class Turn
 
   def initialize(number, currentPlayer)
     @squareNumber = number
-    @value1 = ' '
-    @value2 = ' '
+    @value1 = 0
+    @value2 = 0
     @marker = currentPlayer.marker
+  end
+
+  def numberConverter()
+    if @squareNumber == 1
+      @value1 = 0
+      @value2 = 0
+    elsif @squareNumber == 2
+      @value1 = 0
+      @value2 = 1
+    elsif @squareNumber == 3
+      @value1 = 0
+      @value2 = 2
+    elsif @squareNumber == 4
+      @value1 = 1
+      @value2 = 0
+    elsif @squareNumber == 5
+      @value1 = 1
+      @value2 = 1
+    elsif @squareNumber == 6
+      @value1 = 1
+      @value2 = 2
+    elsif @squareNumber == 7
+      @value1 = 2
+      @value2 = 0
+    elsif @squareNumber == 8
+      @value1 = 2
+      @value2 = 1
+    elsif @squareNumber == 9
+      @value1 = 2
+      @value2 = 2
+    else
+      puts "Please pick a number between 1 and 9"
+    end
   end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,20 +2,27 @@ require 'turn'
 
 describe Turn do
   let(:player) { double name: 'Bertie', marker:'X'}
-  subject(:Turn) { described_class.new('5', player) }
+  subject(:turn) { described_class.new(5, player) }
   it { is_expected.to be_a Turn}
 
   it 'is initalized with a number between 1-9' do
-    expect(subject.squareNumber).to eq('5')
+    expect(subject.squareNumber).to eq(5)
   end
 
   it 'has empty spaces for values to be added into' do
-    expect(subject.value1).to eq(' ')
-    expect(subject.value2).to eq(' ')
+    expect(subject.value1).to eq(0)
+    expect(subject.value2).to eq(0)
   end
 
   it 'knows the current Players mark' do
     expect(subject.marker).to eq('X')
   end
 
+  describe '#numberConverter' do
+    it 'Converts a number into the array reference of that square' do
+      turn.numberConverter()
+      expect(subject.value1).to eq(1)
+      expect(subject.value2).to eq(1)
+    end
+  end
 end
